011a32bdd9e2768120c2b039dc5c878c86bddb2c
wind86 <wind86@meta.ua>
2017-12-17 10:52:51 +0200

    create simple application

---
+++ src/main/java/com/transaction/processor/Application.java
@@ -0,0 +1,15 @@
+package com.transaction.processor;
+
+import static org.springframework.boot.SpringApplication.run;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.transaction.processor.Application;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		run(Application.class, args);
+	}
+}
