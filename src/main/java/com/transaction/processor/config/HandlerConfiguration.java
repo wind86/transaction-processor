389032ac82fbb3ebce789aac2b6f1ae144ba47c1
wind86 <wind86@meta.ua>
2017-12-20 03:10:23 +0200

    fix maven build

--- src/main/java/com/transaction/processor/config/HandlerConfiguration.java
+++ src/main/java/com/transaction/processor/config/HandlerConfiguration.java
@@ -1,6 +1,6 @@
 package com.transaction.processor.config;
 
-import static com.transaction.processor.config.HandlerConfiguration.SpringExtension.SPRING_EXTENSION_PROVIDER;
+import static com.transaction.processor.handler.extension.SpringExtension.SPRING_EXTENSION_PROVIDER;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -8,13 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import akka.actor.AbstractExtensionId;
-import akka.actor.Actor;
 import akka.actor.ActorSystem;
-import akka.actor.ExtendedActorSystem;
-import akka.actor.Extension;
-import akka.actor.IndirectActorProducer;
-import akka.actor.Props;
 
 //Inspired by http://www.baeldung.com/akka-with-spring
 
@@ -32,52 +26,5 @@ public class HandlerConfiguration {
 		ActorSystem system = ActorSystem.create(TRANSACTION_HANDLER);
 		SPRING_EXTENSION_PROVIDER.get(system).initialize(applicationContext);
 		return system;
-	}
-
-	public static class SpringActorProducer implements IndirectActorProducer {
-
-		private ApplicationContext applicationContext;
-
-		private String beanActorName;
-
-		public SpringActorProducer(ApplicationContext applicationContext, String beanActorName) {
-			this.applicationContext = applicationContext;
-			this.beanActorName = beanActorName;
-		}
-
-		@Override
-		public Actor produce() {
-			return (Actor) applicationContext.getBean(beanActorName);
-		}
-
-		@Override
-		@SuppressWarnings("unchecked")
-		public Class<? extends Actor> actorClass() {
-			return (Class<? extends Actor>) applicationContext.getType(beanActorName);
-		}
-	}
-
-	public static class SpringExtension extends AbstractExtensionId<SpringExtension.SpringExt> {
-
-		public static final SpringExtension SPRING_EXTENSION_PROVIDER = new SpringExtension();
-
-		@Override
-		public SpringExt createExtension(ExtendedActorSystem system) {
-			return new SpringExt();
-		}
-
-		public static class SpringExt implements Extension {
-
-			private volatile ApplicationContext applicationContext;
-
-			public void initialize(ApplicationContext applicationContext) {
-				this.applicationContext = applicationContext;
-			}
-
-			public Props props(String actorBeanName) {
-				return Props.create(SpringActorProducer.class, applicationContext, actorBeanName);
-			}
-		}
-	}
-	
+	}	
 }
--- src/main/java/com/transaction/processor/controller/AbstractController.java
+++ src/main/java/com/transaction/processor/controller/AbstractController.java
@@ -1,13 +1,13 @@
 package com.transaction.processor.controller;
 
-import static com.transaction.processor.config.HandlerConfiguration.SpringExtension.SPRING_EXTENSION_PROVIDER;
+import static com.transaction.processor.handler.extension.SpringExtension.SPRING_EXTENSION_PROVIDER;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 
 public abstract class AbstractController {
 
-	protected ActorRef getHandlerActor(ActorSystem system, String actorId/*, String actorName*/) {
-		return system.actorOf(SPRING_EXTENSION_PROVIDER.get(system).props(actorId)/*, actorName*/);
+	protected ActorRef getHandlerActor(ActorSystem system, String actorId) {
+		return system.actorOf(SPRING_EXTENSION_PROVIDER.get(system).props(actorId));
 	}
 }
